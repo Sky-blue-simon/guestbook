62525856801f661dde68e1861a781bc96f83eda5
Sky-blue-simon <80744224+Sky-blue-simon@users.noreply.github.com>
2024-01-07 13:36:53 +0900

    Update PostTest.java

--- src/test/java/com/jadecross/guestbook/PostTest.java
+++ src/test/java/com/jadecross/guestbook/PostTest.java
@@ -8,6 +8,6 @@ class PostTest {
   void testSetName() {
     Post post = new Post("TESTER", "2022-04-12 01:01:48", "Happy Wedding");
     post.setName("DEVOPS");
-    assertEquals("DEVOPS", post.getName();
+    assertEquals("DEVOPS", post.getName());
   }
 }
